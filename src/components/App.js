3762a28fcb196a45ff0391f6e6b88a828b648235
ashleyannlaz <paperpastels@gmail.com>
2021-07-13 17:52:58 -0400

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,41 @@
 // This is the top-level component
 // so we'll keep application state at this level.
 // 👉 1- Import the state hook!
-import React from 'react'
+import React, { useState } from 'react'
 import FriendsList from './FriendsList'
 import Search from './Search'
 // 👉 2- Import the dummy data that will power the application.
 // (Tomorrow we'll fetch the data from an API instead.)
-
+import friends, { hello, goodbye }  from '../dummy-data/friends'
 
 export default function App() {
   // 👉 3- Initialize a slice of state to keep track of the data
   // using the dummy data as the initial value of the slice of state
+const [myFriends, setMyFriends] = useState(friends)
+
+// this data is appointments on such and so data
 
   // 👉 4- Initialize a slice to keep track of the value of the search box
   // using an empty string as the initial value of the slice
-
+  const [search, setSearch] = useState('')
   // 👉 5- Build a `changeStatus` function that takes an id and
   // changes the `married` from true to false and viceversa
+// function changeStatus(id) {
+//   setMyFriends(myFriends.map(friend => {
+//     // if the friend has the given id, return a copy of the friend object which has the toggled civil status, otherwise return the friend unchanged
+//     if (friend.id === id) {
+//       return { ...friend, married: !friend.married }
+//   }
+//   return friend
+// }))
+
+function changeStatus(id) {
+  setMyFriends(myFriends.map(friend => {
+    return friend.id === id
+    ? { ...friend, married: !friend.married }
+    : friend
+  }))
+}
 
   // STRETCH - Make a helper function that returns
   // a filtered array of friends data (filtering by search term)
@@ -24,9 +43,11 @@ export default function App() {
   return (
     <div className='app-friends container'>
       {/* 👉 6- Render the Search component */}
+      <Search />
       {/* STRETCH - Changes to the input should update the search term */}
 
       {/* 👉 7- Render the FriendsList component */}
+      <FriendsList friends={myFriends} changeStatusFunc={changeStatus} />
       {/* What prop/props does FriendsList need? */}
     </div>
   )
--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,12 +1,26 @@
 import React from 'react'
-// 👉 1- FriendsList renders several Friend components, import Friend
+// 👉 1- FriendsList renders several Friend components, import 
+import Friend from './Friend'
 
 export default function FriendsList(props) {
+  const { friends, changeStatusFunc } = props
   // 👉 2- What data does FriendsList need to do its job? Use destructuring
+  
   return (
     <div className='list-friends container'>
       {/* 👉 3- We need to loop over the data rendering a Friend as we go */}
       {/* Each friend is going to need a `key` prop and also some other prop with data */}
+      {friends.map((friendObj) => {
+        return <Friend 
+        key={friendObj.id}
+        friendDetails={friendObj} 
+        changeCivilStatus={changeStatusFunc}
+        // name={friendObj.name}
+        // age={friendObj.age}
+        // married={friendObj.married}
+        />
+
+      })}
     </div>
   )
 }
